884bd22accaedb72aa31cf9910675b1aafb6ed15
angelHG1 <133866645+angelHG1@users.noreply.github.com>
2026-04-09 08:06:32 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using static Calculadora_delegados.Operaciones;
+
+namespace Calculadora_delegados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2;
+            int opcion;
+          
+            Console.Write("Ingrese el primer número:");
+            num1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo número:");
+            num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Operacion op = null;
+        Menu:
+            Console.WriteLine("Ingrese la operación a realizar");
+            Console.WriteLine("1. Sumar");
+            Console.WriteLine("2. Restar");
+            Console.WriteLine("3. Multiplicar");
+            Console.WriteLine("4. Dividir");
+            Console.WriteLine("5. Potencia");
+            Console.Write("---> ");
+            opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    op = Operaciones.Sumar;
+                    break;
+                case 2:
+                    op = Operaciones.Restar;
+                    break;
+                case 3:
+                    op = Operaciones.Multiplicar;
+                    break;
+                case 4:
+                    op = Operaciones.Dividir;
+                    break;
+                case 5:
+                    op = Operaciones.Potencia;
+                    break;
+                default:
+                    Console.WriteLine("Opción inválida");
+                    Console.Clear();
+                    goto Menu;
+            }
+            double resultado = op(num1, num2);
+            Console.WriteLine("Resultado: " + resultado);
+        }
+    }
+}
+
---
+++ operaciones.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Calculadora_delegados;
+public class Operaciones
+{
+    public delegate double Operacion(double num1, double num2);
+    public static double Sumar(double num1, double num2)
+    {
+        return num1 + num2;
+    }
+    public static double Restar(double num1, double num2)
+    {
+        return num1 - num2;
+    }
+    public static double Multiplicar(double num1, double num2)
+    {
+        return num1 * num2;
+    }
+    public static double Dividir(double num1, double num2)
+    {
+        if (num2 == 0)
+        {
+            Console.WriteLine("\nERROR No se puede dividir entre cero.");
+            return 0;
+        }
+        return num1 / num2;
+    }
+    public static double Potencia(double num1, double num2)
+    {
+        return Math.Pow(num1, num2);
+    }
+}
